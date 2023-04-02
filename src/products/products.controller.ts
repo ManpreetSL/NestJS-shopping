4fec25bde98b6fd5294b9183de397393904694a3
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2023-04-02 22:51:19 +0100

    feat: add controller methods: get by ID, update and delete

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -18,5 +26,25 @@ export class ProductsController {
   @Get()
   getAllProducts() {
     return this.productsService.getAllProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') productId: string) {
+    return this.productsService.getProductById(productId);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body('title') title: string,
+    @Body('description') description: string,
+    @Body('price') price: number,
+  ) {
+    this.productsService.updateProduct(id, title, description, price);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    this.productsService.deleteProduct(id);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -15,5 +15,38 @@ export class ProductsService {
 
   getAllProducts() {
     return [...this.products];
+  }
+
+  getProductById(id: string) {
+    return { ...[this.findProduct(id)] };
+  }
+
+  updateProduct(id: string, title: string, description: string, price: number) {
+    const [product, index] = this.findProduct(id);
+    const updatedProduct = { ...product };
+
+    if (id) updatedProduct.id = id;
+    if (title) updatedProduct.title = title;
+    if (description) updatedProduct.description = description;
+    if (price) updatedProduct.price = price;
+
+    this.products[index] = updatedProduct;
+  }
+
+  deleteProduct(id: string) {
+    const [_, index] = this.findProduct(id);
+    this.products.splice(index, 1);
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex(
+      (product) => product.id === id,
+    );
+    const product = this.products[productIndex];
+
+    if (!product)
+      throw new NotFoundException(`Could not find product with ID ${id}`);
+
+    return [product, productIndex];
   }
 }
